38b79971fbe3ed30181509f7b756505677e88a4f
YiningChen001586646 <chen.yinin@northeastern.edu>
2022-05-23 13:15:12 -0400

    add front end

--- springbootBackend/src/main/java/net/javaproject/springbootBackend/SpringbootBackendApplication.java
+++ springbootBackend/src/main/java/net/javaproject/springbootBackend/SpringbootBackendApplication.java
@@ -1,13 +1,36 @@
 package net.javaproject.springbootBackend;
 
+import net.javaproject.springbootBackend.model.Employee;
+import net.javaproject.springbootBackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApplication.class, args);
+    }
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Employee employee = new Employee();
+        employee.setFirstName("Yining");
+        employee.setLastName("Chen");
+        employee.setEmailId("yiningchen2018@gmail.com");
+        employeeRepository.save(employee);
+
+        Employee employee1 = new Employee();
+        employee1.setFirstName("Keqiang");
+        employee1.setLastName("Wu");
+        employee1.setEmailId("wkq881124@gmail.com");
+        employeeRepository.save(employee1);
+
+
+    }
 }

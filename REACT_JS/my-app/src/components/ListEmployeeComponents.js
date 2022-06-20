112b16bfc1c2ed343b54920d81eeef651e1ef2b2
YiningChen001586646 <chen.yinin@northeastern.edu>
2022-06-19 22:34:02 -0400

    update

--- REACT_JS/my-app/src/components/ListEmployeeComponents.js
+++ REACT_JS/my-app/src/components/ListEmployeeComponents.js
@@ -1,27 +1,28 @@
- 
+
 import React, { useEffect, useState } from 'react'
 import EmployeeService from '../services/EmployeeService'
-import {Link} from 'react-router-dom' 
+import { Link } from 'react-router-dom'
 
 //rfce ES6 error function
 const ListEmployeeComponents = () => {
     //the useState is a Hook (function) that allows you to have state variabes in functional components
     const [employees, setemployees] = useState([])
     //the useEffect Hook allows us to perform side effects (an action) int the function components
-    useEffect(()=>{
-       EmployeeService.getAllEmployees().then((response)=>{
-           setemployees(response.data)
-           console.log(response.data)
-       }).catch(error => {
-           console.log(error);
-       })
-    },[])
+    useEffect(() => {
+        EmployeeService.getAllEmployees().then((response) => {
+            setemployees(response.data)
+            console.log(response.data)
+        }).catch(error => {
+            console.log(error);
+        })
+    }, [])
 
     return (
-        <div className = "container">
-            <h2 className = "text-center ">List Employees</h2>
-            <Link to ="/add-employee" className = "btn btn-primary mb-2"> Add Employee </Link>
-            <table className = "table table-bordered table-striped">
+        <div className="container">
+            <h2 className="text-center ">List Employees</h2>
+            <div className="row"></div>
+            <Link to="/add-employee" className="btn btn-primary mb-2" onClick={this.add}> Add Employee </Link>
+            <table className="table table-bordered table-striped">
                 <thead>
                     <th>Employee Id</th>
                     <th>Employee First Name</th>
@@ -31,13 +32,13 @@ const ListEmployeeComponents = () => {
                 <tbody>
                     {
                         employees.map(
-                            employee => 
-                            <tr key = {employee.id}>
-                                <td> {employee.id} </td>
-                                <td> {employee.firstName} </td>
-                                <td> {employee.lastName} </td>
-                                <td> {employee.emailId} </td>
-                            </tr>
+                            employee =>
+                                <tr key={employee.id}>
+                                    <td> {employee.id} </td>
+                                    <td> {employee.firstName} </td>
+                                    <td> {employee.lastName} </td>
+                                    <td> {employee.emailId} </td>
+                                </tr>
                         )
                     }
 
@@ -46,9 +47,9 @@ const ListEmployeeComponents = () => {
 
             </table>
 
-      
+
         </div>
-  )
+    )
 }
 
 export default ListEmployeeComponents
